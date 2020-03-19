59321d4d6213b4a39a6a2300e9fdf7e95711be3c
John Doe <r0706426@student.thomasmore.be>
2020-03-19 16:49:48 +0100

    start-up-profile.php

---
+++ profile/profile.php
@@ -0,0 +1,12 @@
+<?php
+
+//pofiel aanpassen is mogelijk
+//opladen van foto / avatar
+//Beperk de bestandstypes en bestandsgrootte
+//Geef gebruiksvriendelijke feedback als de update mislukt, maar ook als die lukt
+//Beschrijving / korte profieltekst (emoji’s bewaren moet mogelijk zijn)
+//wachtwoord wijzigen lukt
+//Wat is een veilige manier om dit te doen? Overleg met je team.
+//email adres wijzigen
+//Hoe kan je dit veilig toelaten? (wat als je even van je laptop weg bent en iemand je wachtwoord wijzigt?) Idem voor wachtwoord wijzigen.
+//Zorg dat je hier aantoont dat je hebt nagedacht over een veilige procedure 
