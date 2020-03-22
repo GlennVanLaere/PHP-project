770914b51381ed2cef6d5a28fb3b9bae5ff97c6c
John Doe <r0706426@student.thomasmore.be>
2020-03-22 20:23:21 +0100

    upload_foto
    issue:
    -> no checkmarks for type/size.
    -> no solution yet

--- profile/profile.php
+++ profile/profile.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once(__DIR__."/functions/avatarUpload.php");
 //pofiel aanpassen is mogelijk
 //opladen van foto / avatar
 //Beperk de bestandstypes en bestandsgrootte
@@ -10,3 +10,24 @@
 //email adres wijzigen
 //Hoe kan je dit veilig toelaten? (wat als je even van je laptop weg bent en iemand je wachtwoord wijzigt?) Idem voor wachtwoord wijzigen.
 //Zorg dat je hier aantoont dat je hebt nagedacht over een veilige procedure 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    <div class="content">
+        <form action="" method="post" enctype="multipart/form-data" >
+            <input type="file" name="avatar">
+            <button type="submit" name="submit">Upload profile picture</button>
+
+        </form>
+    
+        <h1> <?php echo $error ?> </h1>
+    
+    </div>
+</body>
+</html>
