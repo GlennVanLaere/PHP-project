1863ab373f6a217182236721efe0ed7884394564
John Doe <r0706426@student.thomasmore.be>
2020-03-23 12:51:32 +0100

    working uploadToRootfolder
    working upload system to rootfolder "uploads"

---
+++ classes/avatarUpload.php
@@ -0,0 +1,41 @@
+<?php 
+include_once(__DIR__."/functions/settings.php");
+
+if(isset($_POST["submit"])){
+    $file = $_FILES["avatar"];
+    $fileName = $_FILES["avatar"]["name"];
+    $fileError = $_FILES["avatar"]["error"];
+    $fileType = $_FILES["avatar"]["error"];
+    $fileTmpName = $_FILES["avatar"]["tmp_name"];
+    $fileSize = $_FILES["avatar"]["size"];
+
+    $fileExt = explode(".", $fileName);
+    $fileExtention = strtolower(end($fileExt));
+    
+    $allowedFiles = array('jpg', 'jpeg','png');
+
+    if(in_array($fileExtention, $allowedFiles)){
+        if($fileError === 0){
+            if($fileSize < 1000000){
+                $fileNameNew = uniqid('', true).".".$fileExtention;
+                $fileDestination = '/Users/glennvanlaere/Documents/GitHub/PHP-project/uploads/'.$fileNameNew;
+                echo($fileTmpName);
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: profile.php?succes");
+            }
+            else{
+                $error = "your filesize is to big";
+            }
+        }
+        else{
+            $error = "something went wrong";
+        }
+    }
+    else{
+        $error = "we do not support this image type. please 
+        use jpg, jpeg or png";
+    }
+ 
+    
+
+}
---
+++ profile.php
@@ -0,0 +1,41 @@
+<?php
+include_once(__DIR__."/classes/avatarUpload.php");
+
+
+//pofiel aanpassen is mogelijk
+//opladen van foto / avatar
+//Beperk de bestandstypes en bestandsgrootte
+//Geef gebruiksvriendelijke feedback als de update mislukt, maar ook als die lukt
+//Beschrijving / korte profieltekst (emoji’s bewaren moet mogelijk zijn)
+//wachtwoord wijzigen lukt
+//Wat is een veilige manier om dit te doen? Overleg met je team.
+//email adres wijzigen
+//Hoe kan je dit veilig toelaten? (wat als je even van je laptop weg bent en iemand je wachtwoord wijzigt?) Idem voor wachtwoord wijzigen.
+//Zorg dat je hier aantoont dat je hebt nagedacht over een veilige procedure 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Profile</title>
+</head>
+<body>
+    <div class="content">
+        <form action="" method="post" enctype="multipart/form-data" >
+            <input type="file" name="avatar">
+            <button type="submit" name="submit">Upload profile picture</button>
+
+        </form>
+    
+        <h1> <?php echo $error ?> </h1>
+    
+    </div>
+    <title>Uw profiel</title>
+</head>
+<body>
+    
+</body>
+</html>
