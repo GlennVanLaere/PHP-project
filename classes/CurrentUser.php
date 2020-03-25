8ca708c3f5da3e532b289fc11cb86a68814a484b
TERRAKID <wannes.verboven@outlook.com>
2020-03-25 15:45:43 +0100

    [feat] Feature 2: Make getter and setter for current user

---
+++ classes/CurrentUser.php
@@ -0,0 +1,90 @@
+<?php
+    include_once(__DIR__ . "/Db.php");
+
+    class currentUser {
+        private $currentEmail;
+        private $currentFirstName;
+        private $currentLastName;
+        private $currentPassword;
+
+        /**
+         * Get the value of currentEmail
+         */ 
+        public function getCurrentEmail()
+        {
+                return $this->currentEmail;
+        }
+
+        /**
+         * Set the value of currentEmail
+         *
+         * @return  self
+         */ 
+        public function setCurrentEmail($currentEmail)
+        {
+                $this->currentEmail = $currentEmail;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of currentFirstName
+         */ 
+        public function getCurrentFirstName()
+        {
+                return $this->currentFirstName;
+        }
+
+        /**
+         * Set the value of currentFirstName
+         *
+         * @return  self
+         */ 
+        public function setCurrentFirstName($currentFirstName)
+        {
+                $this->currentFirstName = $currentFirstName;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of currentLastName
+         */ 
+        public function getCurrentLastName()
+        {
+                return $this->currentLastName;
+        }
+
+        /**
+         * Set the value of currentLastName
+         *
+         * @return  self
+         */ 
+        public function setCurrentLastName($currentLastName)
+        {
+                $this->currentLastName = $currentLastName;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of currentPassword
+         */ 
+        public function getCurrentPassword()
+        {
+                return $this->currentPassword;
+        }
+
+        /**
+         * Set the value of currentPassword
+         *
+         * @return  self
+         */ 
+        public function setCurrentPassword($currentPassword)
+        {
+                $this->currentPassword = $currentPassword;
+
+                return $this;
+        }
+    }
+?>
--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+  include_once(__DIR__."/classes/CurrentUser.php");
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
