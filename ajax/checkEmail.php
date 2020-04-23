54141f894ac238afee45422c0e60da1c3e154d09
Amber <amber.waltens@hotmail.com>
2020-04-23 16:23:16 +0200

    adjusted checkEmail.php if isset added

--- ajax/checkEmail.php
+++ ajax/checkEmail.php
@@ -1,21 +1,25 @@
 <?php
-    include_once(__DIR__ . "/../classes/User.php");
-       
-    session_start();
-    $user = new User();
-    $user->setEmail($_POST['email']);
-    
-    $user->checkEmail($email);
-    if($res){
-        $response = [
-            "available" => "Email is not available"
-        ];
-    }else{
-        $response = [
-            "available" => "Email is available"
-        ];
-    }
+    include_once( __DIR__ . '/../classes/User.php' );
 
-        header('Content-Type: application/json');
-        echo json_encode($response);
+    if ( isset( $_POST['email'] ) )
+    {
+
+        $email = $_POST['email'];
+        $user = new User();
+        $res = $user->checkEmail( $email );
+        $user->setEmail($email);
+
+        if ( $res ) {
+            $response = [
+                'available' => 'Email is not available'
+            ];
+        } else {
+            $response = [
+                'available' => 'Email is available'
+            ];
+        }
+
+        header( 'Content-Type: application/json' );
+        echo json_encode( $response );
+    }
 ?>
