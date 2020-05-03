7f9c3b8e15afa517931f0c54dceede42b1fe625e
Gregory <r0613527@student.thomasmore.be>
2020-05-03 17:44:21 +0200

    update css
    add css

--- js/liveSearch.js
+++ js/liveSearch.js
@@ -1,6 +1,7 @@
 document.querySelector("#searchBox").addEventListener("keyup", function() {
     let category = document.querySelector("#category").value;
     let searchTerm = document.querySelector("#searchBox").value;
+    let changeIt = document.getElementById('searchResults');
     document.getElementById('searchResults').innerHTML = "";
     if (searchTerm != "") 
     {
@@ -13,8 +14,11 @@ document.querySelector("#searchBox").addEventListener("keyup", function() {
             },
             dataType: "JSON" 
         }).done(function(res) {
-            for(i=0;i<res.length; i++){
-                document.getElementById('searchResults').innerHTML += '<a href="public.php?id='+ res[i].id + '">' + res[i].firstName+'</a>';
+            if( res.lenght != 0 ){
+                changeIt.style.visibility = 'visible';
+                for(i=0;i<res.length; i++){
+                    document.getElementById('searchResults').innerHTML += '<a href="public.php?id='+ res[i].id + '" class="searchItem">' + res[i].firstName + ' -> go to profile' + '</a>';
+                }
             }
         });
     }
