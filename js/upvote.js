743f432bfb9578fda25d32ea8203d96c5e5bb827
Amber <amber.waltens@hotmail.com>
2020-05-03 08:50:05 +0200

    cs fixer

--- js/upvote.js
+++ js/upvote.js
@@ -1,38 +1,38 @@
 let btns = document.querySelectorAll(".click")
 
-btns.forEach((btn)=>{
+btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         console.log("click upvote")
         upvote()
         e.preventDefault()
     })
 
-    function upvote(){ 
-        
+    function upvote() {
+
         let id = btn.dataset.id
         console.log(id)
-        
+
         const formData = new FormData()
-    
         formData.append("id", id)
-    
+
         fetch('ajax/upvote.php', {
             method: 'POST',
             body: formData
         })
-        .then((response) => response.json())
-        .then((result) => {
-            console.log(result);
-            if(result.status == "succes"){
-                
-                console.log(result.currentUpvotes)
-                btn.innerHTML = result.currentUpvotes + " upvotes"
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error)
-        })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+                if (result.status == "succes") {
+                    console.log(result.currentUpvotes)
+                    btn.innerHTML = result.currentUpvotes + " upvotes"
+                } else if (result.status == "fail") {
+                    console.log(result.message)
+                    btn.innerHTML = result.currentUpvotes + " upvotes"
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error)
+            })
     }
 
 })
-
